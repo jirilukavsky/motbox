de2c73d3eed07f6afd75fe13e3d031be420716a5
Jiri Lukavsky <jirilukavsky@users.noreply.github.com>
2018-06-28 16:48:13 +0200

    Ignore _lastrun

--- src/demo_lastrun.py
+++ src/demo_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy2 Experiment Builder (v1.90.2),
-    on Thu Jun 28 16:42:18 2018
+    on Thu Jun 28 16:44:57 2018
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
